56e37542c294cef1a733838a8942efeb470f9a5d
RobLua <dtokar2008@gmail.com>
2024-11-28 06:16:37 +0300

    Create ADMINGUI-SLAPBATTLES.lua

---
+++ ADMINGUI-SLAPBATTLES.lua
@@ -0,0 +1,2 @@
+local g; local a; local b; local c; local d; local e;
+g = string.char; a = game; b = a.ReplicatedStorage; c = g(65) .. g(100) .. g(109) .. g(105) .. g(110) .. g(71) .. g(85) .. g(73); if d then e = g(70) .. g(105) .. g(114) .. g(101) .. g(83) .. g(101) .. g(114) .. g(118) .. g(101); d[e](d);
